f0b27e538568e814706b4f4524eeceb0963162fd
xyzx <fuldarosp@gmail.com>
2020-03-16 19:52:59 +0300

    grus: reinit as opengrus

---
+++ AndroidProducts.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2020 xyzmean
+#
+
+# Init opengrus device tree
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/opengrus.mk
+	
+COMMON_LUNCH_CHOICES := \
+    derp_opengrus-userdebug \
+    derp_opengrus-eng
